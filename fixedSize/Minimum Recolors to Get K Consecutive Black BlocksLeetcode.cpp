3af493a48e07934e664bfff2e1049f05c96e9f10
vasubansal27 <111572815+vasubansal27@users.noreply.github.com>
2022-08-21 18:37:09 +0530

    Create  Minimum Recolors to Get K Consecutive Black BlocksLeetcode.cpp

---
+++ fixedSize/Minimum Recolors to Get K Consecutive Black BlocksLeetcode.cpp
@@ -0,0 +1,28 @@
+int minimumRecolors(string blocks, int k) {
+    int i=0,j=0;
+    int white=0;
+    int n=blocks.size();
+    int ans=INT_MAX;
+    while(j<n)
+    {
+        if(j-i+1<k)
+        {
+            if(blocks[j]=='W')
+            white++;
+        }
+        else if(j-i+1==k)   
+        {
+            //jb window size hit krega tb firse check krnege na j ko
+            if(blocks[j]=='W')
+            white++;
+            ans=min(ans,white);
+            if(blocks[i]=='W')
+            white--;
+            i++;
+        }
+        j++;
+    }
+    if(ans==INT_MAX)
+        return 0;
+return ans;
+}
